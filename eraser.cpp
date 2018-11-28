24a57d20c22716e22a9d73daa21ad8f91fb810a0
shengyang998 <shengyang998@163.com>
2018-11-28 17:34:26 +0800

    ## improvement: 1. this program now will wirte random data to file\n2. improve the calculation speed by replacing  and  operators with bitwise shifting\n\n## bugfix: memory leak

--- eraser.cpp
+++ eraser.cpp
@@ -1,25 +1,29 @@
+//
+//  main.cpp
+//  eraser
+//
+//  Created by Ysy on 2018/11/28.
+//  Copyright © 2018 Ysy. All rights reserved.
+//
+
 #include<iostream>
 #include<fstream>
 #include<string>
 #include<sstream>
+#include<ctime>
+
+
+#define LL long long
 
 const int buffer_size = 4096;
 
 using namespace std;
 
-long long getSizeOfFile(ofstream& file){
+LL getSizeOfFile(ofstream& file){
     file.seekp(0, file.end);
-    long long sizeOfFile = file.tellp();
+    LL sizeOfFile = file.tellp();
     file.seekp(0, file.beg);
     return sizeOfFile;
-}
-
-int setContent(char content[], int size){
-    for(int i=0;i<size;i++){
-        //content[i] = content[i]*content[i+size]*(~content[i+2*size]);
-        content[i] = content[i+size] & content[i+2*size];
-    }
-    return 0;
 }
 
 int toInt(char ch[]){
@@ -32,6 +36,16 @@ int toInt(char ch[]){
         strStream >> result;
     }
     return result;
+}
+
+void initRandomArray(char arr[], const int len){
+    for (int i=0; i<len; i++){
+        srand((unsigned int)time(nullptr));
+        int ra = rand();
+        arr[i] = ra - (ra >> 6 << 6); // ra % 128;
+//        cout<<"random number: "<<ra<<". random char: "<<arr[i]<<endl;
+    }
+
 }
 
 int main(int argc, char* argv[]){
@@ -60,68 +74,69 @@ int main(int argc, char* argv[]){
     if (argc==1) {//zero para
         cout<<"Please input file name: ";
         while(!(cin>>fileName))
-            cout<<"Error in file name, please input again: ";cin.ignore(255, '\n');cin.clear();
+            cerr<<"Error in file name, please input again: "; cin.ignore(255, '\n'); cin.clear();
     }
-    else if(argc > 1) {//one para, assume it as file name
+    else if(argc > 1) {//get one parameter, assuming it it file name
         fileName = argv[1];
-        if(argc > 2){//two paras, assume it as file name and times
+        if(argc > 2){//get two parameters, assuming it is file name and times
             times = toInt(argv[2]);
         }
     }
     if(times <= 0){
         cout<<"How much times you want to cover: ";
         while(!(cin>>times)){
-            cout<<"Error, please input again: ";cin.ignore(255, '\n');cin.clear();
+            cerr<<"Error, please input again: "; cin.ignore(255, '\n'); cin.clear();
         }
     }
     //create the fileStream
     ofstream fileOut;
     //open the file
     fileOut.open(fileName, ios::app|ios::binary);
-    fileOut.seekp(0,ios::beg);
+    fileOut.seekp(0, ios::beg);
     //get size of file
-    long long sizeOfFile = getSizeOfFile(fileOut);
+    LL sizeOfFile = getSizeOfFile(fileOut);
     cout<<"The size of the file is: "<<sizeOfFile<<endl;
     cout<<"The file will be covered "<<times<<" times."<<endl;
     cout<<"covering data..."<<endl;
     //prepare the data to cover
-    char* content = new char[1];
-
-    for(int i=0;i<times;i++){
+    char* atom = new char[1];
+    
+    for(int i=0; i<times; i++){
         //write the file
-        fileOut.seekp(0, fileOut.beg);
+        fileOut.seekp(0, ios::beg);
+        LL blocksOfFile = sizeOfFile>>12 ;// sizeOfFile/buffer_size;
         //write with content
-        for(long long j=0;j<(sizeOfFile/buffer_size);j+=buffer_size){
+        for(LL j=0; j < blocksOfFile; j += buffer_size){
             //delete the old content and alloc new content
-            //delete[] content;
-            content = new char[buffer_size];
-            //setContent(content, buffer_size);
-
-            fileOut.write(content, buffer_size);
+            delete[] atom;
+            atom = new char[buffer_size];
+            initRandomArray(atom, buffer_size);
+            fileOut.write(atom, buffer_size);
             // IF I SHOULD DELETE THE PLUS ONE?
             fileOut.seekp((j+1)*buffer_size);
             if(fileOut.tellp() == fileOut.end){
+                cerr<<"break the loop with tellp == file.end"<<endl;
                 break;
             }
         }
     }
     //close the file
     fileOut.close();
-
+    
     //clean the file
     //fileOut.open(fileName);
     //fileOut.close();
-
+    
     //clean the mem
-    delete[] content;
-    content = nullptr;
-
+    delete[] atom;
+    atom = nullptr;
+    
     //delete the file
     if(0 == remove(fileName.c_str())){
         cout<<"File is erased."<<endl;
     }else{
         perror("Error erasing file");
     }
-
+    
     return 0;
 }
